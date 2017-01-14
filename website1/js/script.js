c7c498f13208826f1c315c42e8406f952180d63b
shawofomaha <markshaw.us@gmail.com>
2017-01-14 12:25:07 -0600

    Update script.js

--- website1/js/script.js
+++ website1/js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
-  $("p").fadeIn(5000);
-  $("img").fadeIn(3000);
+  $("button").click(function(){
+    $("p").fadeIn(5000);
+    $("img").fadeIn(3000);
+  });
+  
 });
